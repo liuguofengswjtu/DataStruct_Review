ec392f126dd43f67bfd16c0b133803b12082735c
lgf <2475726772@qq.com>
2022-02-13 18:08:46 +0800

    修改Kmp算法，因为strlen返回的size_t类型和int类型进行比较会出现一些错误

--- KMP_Algorithm.cpp
+++ KMP_Algorithm.cpp
@@ -4,14 +4,17 @@
 using namespace std;
 void Next(char* p_string, int next[20])
 {
-    int length = strlen(p_string);
+    size_t length = strlen(p_string);
     next[0] = -1;
     int k = -1, j = 0;
     while (j < length - 1)
     {
         if (k == -1 || p_string[j] == p_string[k])
         {
-            next[j++] = k++;
+            j++;
+            k++;
+            next[j] = k;
+            printf("(1) j=%d,k=%d,next[%d]=%d\n", j, k, j, k);
         }
         else
             k = next[k];
@@ -19,11 +22,16 @@ void Next(char* p_string, int next[20])
 }
 int Kmp(char* t_string, char* p_string)
 {
-    int next[100], i = 0, j = 0;
+    int next[20];
+    int i = 0, j = 0;
+    //下面的len1,len2并不能改成size_t，否则和Int型的i,j比较大小时会出现错误
+    //例如abcdef和cde匹配时，cde应在主串的第二个位置(0,1,2),但是用size_t时会
+    //出现返回值等于-3的错误
+    int len1 = 0,len2 = 0;
     memset(next, 0, sizeof(next));
     Next(p_string, next);
-    int len1=strlen(t_string);
-    int len2=strlen(p_string);
+    len1=strlen(t_string);
+    len2=strlen(p_string);
     while (i < len1 && j < len2)
     {
         if (j == -1 || t_string[i] == p_string[j])
@@ -34,7 +42,7 @@ int Kmp(char* t_string, char* p_string)
         else
             j = next[j];
     }
-    if (j >=len2)
+    if (j >= len2)
         return (i - len2);
     else
         return -1;
@@ -42,14 +50,14 @@ int Kmp(char* t_string, char* p_string)
 int main()
 {
     int index = 0;
-    char *t_string = new char[20];
+    char* t_string = new char[20];
     char* p_string = new char[20];
     char in;
     int i = 0, j = 0;
     printf("Please input the first string:\n");
-    cin.getline(t_string,20);
+    cin.getline(t_string, 20);//get_s也可以,只不过只能在msvc编译器上通过
     printf("Please input the pattern string:\n");
-    cin.getline(p_string,20);
+    cin.getline(p_string, 20);
     index = Kmp(t_string, p_string);
     cout << p_string << " is at the " << index << "th " << "position of " << t_string << endl;
     return 0;
--- 二叉树及其操作.cpp
+++ 二叉树及其操作.cpp
@@ -3,13 +3,19 @@
 using namespace std;
 /*
 input data:
-10
-5 3 7 1 4 6 8 0 2 9
+11
+5 3 7 1 4 6 8 0 2 9 10
+To test whether the order of input data can effect the struct of the binary tree:
+11
+5 3 1 0 2 4 6 7 8 9 10
 these data will found a binary tree like this sketch.
         5
      3     7
    1  4   6  9
   0 2       8 10
+The result of PreorderTraversal is:5 3 1 0 2 4 7 6 9 8 10
+The result of InorderTraversal is:0 1 2 3 4 5 6 7 8 9 10
+The result of PostorderTraversal is:0 2 1 4 3 6 8 10 9 7 5
 */
 typedef int ElementType;
 //节点定义
@@ -71,20 +77,71 @@ void Insert(Tree* tree, ElementType value)
     //插入结束
     return;
 }
-//递归中序遍历,可以将构造的二叉搜索树升序输出
-void InoderTraversal(TreeNode* treeNode)
+//递归先序遍历
+void PreorderTraversal(TreeNode* treeNode)
 {
     //如果treeNode为空，则回退上一节点
 
     if (treeNode != NULL)
     {
-        InoderTraversal(treeNode->left);
         cout << "Node->data= " << treeNode->data << endl;
-        InoderTraversal(treeNode->right);
+        PreorderTraversal(treeNode->left);
+        PreorderTraversal(treeNode->right);
+    }
+}
+//递归中序遍历,可以将构造的二叉搜索树升序输出
+void InorderTraversal(TreeNode* treeNode)
+{
+    //如果treeNode为空，则回退上一节点
+
+    if (treeNode != NULL)
+    {
+        InorderTraversal(treeNode->left);
+        cout << "Node->data= " << treeNode->data << endl;
+        InorderTraversal(treeNode->right);
+    }
+}
+//递归后序遍历
+void PostorderTraversal(TreeNode* treeNode)
+{
+    //如果treeNode为空，则回退上一节点
+
+    if (treeNode != NULL)
+    {
+        InorderTraversal(treeNode->left);
+        InorderTraversal(treeNode->right);
+        cout << "Node->data= " << treeNode->data << endl;
+    }
+}
+//非递归先序遍历
+void NonRecursive_PreorderTraversal(TreeNode* treeNode)
+{
+    stack<TreeNode*>NodeStack;
+    TreeNode* tempPointer=treeNode;
+    while(tempPointer!=NULL||!NodeStack.empty())
+    {
+        
+        if(tempPointer!=NULL)
+        {           
+            NodeStack.push(tempPointer);
+            cout<<"Node->data: "<<tempPointer->data<<endl;
+            tempPointer=tempPointer->left;
+        }
+        else
+        //即tempPointer==NULL且NodeStack不等于空，意为左子树遍历到底了
+        //此时需要回退到上一节点(即其左子节点是当前节点的节点)
+        {
+            //使其回退到上一节点
+            NodeStack.pop();
+            //并让tempPointer上一节点，用于访上一节点的右子节点
+            tempPointer=NodeStack.top();
+            //指向右子节点
+            tempPointer=tempPointer->right;
+        }
     }
 }
 //非递归中序遍历
-void NonRecursive_InoderTraversal(TreeNode* treeNode)
+void NonRecursive_InorderTraversal(TreeNode* treeNode)
 {
     stack<TreeNode*>NodeStack;
     TreeNode* tempPointer = treeNode;//用于遍历的指针
@@ -92,7 +149,7 @@ void NonRecursive_InoderTraversal(TreeNode* treeNode)
     {
         if (tempPointer != NULL)
         {
-            NodeStack.push(tempPointer);//
+            NodeStack.push(tempPointer);
             tempPointer = tempPointer->left;
         }
         else
@@ -121,8 +178,21 @@ int main()
         cin >> inputValue;
         Insert(&tree, inputValue);
     }
-    InoderTraversal(tree.Root);
-    cout << "this is NonRecursive output:\n";
-    NonRecursive_InoderTraversal(tree.Root);
+    cout<<"this is the result of PreorderTraversal:\n";
+    PreorderTraversal(tree.Root);
+
+    // cout<<"this is the result of NonRecursive_PreorderTraversal:\n";
+    // NonRecursive_PreorderTraversal(tree.Root);
+
+    cout<<"this is the result of InorderTraversal:\n";
+    InorderTraversal(tree.Root);
+
+    // cout<<"this is the result of NonRecursive_InorderTraversal:\n";
+    // NonRecursive_InorderTraversal(tree.Root);
+    
+    cout<<"this is the result of PostorderTraversal:\n";
+    PostorderTraversal(tree.Root);
+
+    
     return 0;
 }
